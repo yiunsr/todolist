b63af37918e2d9ce9a7300cc26680841e79d93dd
yiunsr <yiunsr@gmail.com>
2018-03-18 11:19:03 +0900

    todo model 추가, 대략적인 UI Layout 추가

--- todos/models.py
+++ todos/models.py
@@ -6,9 +6,9 @@ from accounts.models import User
 class Todo(models.Model):  
     """ Todo  model 로 우선순위, 텍스트, 완료상태, 생성시간, 수정시간, 사용자 데이터가 존재한다. """
     
-    HIGH = 'H'
-    MID = 'M'
-    LOW = 'L'
+    HIGH = '3'
+    MID = '2'
+    LOW = '1'
     PRIORITY_CHOICES = ((HIGH, u'중요'),(MID, u'보통'),(LOW, u'낮음'))
 
     priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default=MID, )
